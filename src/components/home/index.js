11a2fd0c6dbcbe5445e3d1174d6339d3a76b6f9b
yogesh8633 <yogesh.gupta93@rediffmail.com>
2022-12-17 00:10:34 +0530

    homepage issues solved

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,47 +1,30 @@
 const Home = () => {
   return (
     <section id="home" className="hero">
-      <div className="home-slider  owl-carousel">
-        <div className="slider-item ">
-          <div className="overlay"></div>
-          <div className="container">
-            <div
-              className="row d-md-flex no-gutters slider-text align-items-end justify-content-end"
-              data-scrollax-parent="true"
-            >
-              <div
-                className="one-third js-fullheight order-md-last img"
-                style={{ backgroundImage: "url('assets/images/bg_1.png')" }}
-              >
-                <div className="overlay"></div>
-              </div>
-              <div
-                className="one-forth d-flex  align-items-center ftco-animate"
-                data-scrollax=" properties: { translateY: '70%' }"
-              >
-                <div className="text">
-                  <span className="subheading">Hello!</span>
-                  <h1 className="mb-4 mt-3">
-                    I'm <span>Yogesh Gupta</span>
-                  </h1>
-                  <h2 className="mb-4">A Freelance Web Designer</h2>
-                  <p>
-                    <a
-                      href="#contact"
-                      className="btn btn-primary py-3 px-4"
-                    >
-                      Hire me
-                    </a>{" "}
-                    <a
-                      href="#projects"
-                      className="btn btn-white btn-outline-white py-3 px-4"
-                    >
-                      My works
-                    </a>
-                  </p>
-                </div>
-              </div>
+      <div className="container" style={{marginTop:"15%"}}>
+        <div className="row">
+          <div className="col-lg-6 col-md-12">
+            <div className="text">
+              <span className="subheading">Hello!</span>
+              <h1 className="mb-4 mt-3">
+                I'm <span>Yogesh Gupta</span>
+              </h1>
+              <h2 className="mb-4">A Freelance Web Designer</h2>
+              <p>
+                <a href="#contact" className="btn btn-primary py-3 px-4">
+                  Hire me
+                </a>{" "}
+                <a
+                  href="#projects"
+                  className="btn btn-white btn-outline-white py-3 px-4"
+                >
+                  My works
+                </a>
+              </p>
             </div>
+          </div>
+          <div className="col-lg-6 homeImg">
+            <img src="assets/images/bg_1.png" alt ="MY_IMAGE"style={{  height: "150%",marginTop: "-26%"}}/>
           </div>
         </div>
       </div>
